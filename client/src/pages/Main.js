f39fdcb90bc900831fed8306d88af6c94611cf54
gyeongseo944 <85339587+gyeongseo944@users.noreply.github.com>
2023-02-21 21:12:08 +0900

    Revert "[FE][FEAT]:main 화면 구성 질문 컴포넌트 구성"

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,21 +1,7 @@
-import styled from "styled-components";
-import MainList from "../components/Main/MainList";
-import QuestionTitle from "../components/QuestionTitle";
-
-const MainComponent = styled.div`
-  width: 90%;
-  max-width: 1200px;
-  margin: 50px auto;
-  min-height: 100vh;
-`;
+import React from "react";
 
 function Main() {
-  return (
-    <MainComponent>
-      <QuestionTitle TitleContents="All Questions" />
-      <MainList />
-    </MainComponent>
-  );
+  return <div>Main</div>;
 }
 
 export default Main;
